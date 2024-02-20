6ea3c7a01a54343bfc560bf7aeed7c3101b1de20
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-02-20 15:46:38 +0600

    "Initial frontend part"

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
+import cors from 'cors';
 
 import connectToDB from "./connectToDB";
 
@@ -13,6 +14,7 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      TEST
+    </>
+  );
+}
+
+export default App;
