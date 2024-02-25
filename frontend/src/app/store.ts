bb6a7b4447064fb108899b51d7a16defd13ee024
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-02-25 17:57:30 +0600

    "HW#86: Install redux-persist and remodify store"

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,18 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 
 import { artistsReducer } from '../containers/artists/artistsSlice.ts';
 import { albumReducer } from '../containers/albums/albumsSlice.ts';
 import {tracksReducer} from '../containers/tracks/tracksSlice.ts';
 import {usersReducer} from '../containers/users/usersSlice.ts';
+import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const usersPersistConfig = {
+  key: 'music:users',
+  storage: storage,
+  whitelist: ['users'],
+};
+
+const rootReducer = combineReducers({
+  artists: artistsReducer,
+  albums: albumReducer,
+  tracks: tracksReducer,
+  users: persistReducer(usersPersistConfig, usersReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    artists: artistsReducer,
-    albums: albumReducer,
-    tracks: tracksReducer,
-    users: usersReducer,
-  }
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,18 @@
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
-import {store} from './app/store.ts';
-import {BrowserRouter} from 'react-router-dom';
+import { persistor, store } from './app/store.ts';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
       <App/>
     </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
