62ca2285cd991d8a79735d6c5af4b648c6cb1899
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-03-14 18:17:48 +0600

    "HW#91 Check code and find a few console.log, i delete if"

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,2 @@
 export const apiURL = 'http://localhost:8000';
-export const GOOGLE_CLIENT_ID = '239856321858-mh05b97dr37vl1jhbrjd511cci3hmlht.apps.googleusercontent.com';
-console.log(GOOGLE_CLIENT_ID);
+export const GOOGLE_CLIENT_ID = import.meta.env['VITE_GOOGLE_CLIENT_ID'] as string;
